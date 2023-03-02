7314566a6791cbb5e7e22ac1626879cd6dc3843f
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-03-02 06:42:02 +0100

    Adding DB name as output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-output "mongo_db_id" {
+output "id" {
   description = "The ID of the Cosmos DB Mongo Database."
   value       = azurerm_cosmosdb_mongo_database.this.id
+}
+
+output "name" {
+  description = "The name of the Cosmos DB Mongo Database."
+  value       = azurerm_cosmosdb_mongo_database.this.name
 }
